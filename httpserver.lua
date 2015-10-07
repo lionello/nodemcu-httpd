fb7a82530ab373cd6316cbe7458382a4411ceaeb
Lionello Lunesu <lio+git@lunesu.com>
2015-10-07 11:04:53 +0800

    Initial version with GET, PUT, POST, DELETE support

---
+++ httpserver.lua
@@ -0,0 +1,141 @@
+-- Small HTTP server for NodeMCU
+--
+-- PUT creates a new file in flash
+-- GET returns the contents of a file in flash
+-- DELETE remove the file from flash
+-- POST executes the given lua script (may return a function that receives the payload)
+--
+-- TODO: need a way to return flash contents vs. index.html
+
+return function (port)
+
+  local s = net.createServer(net.TCP, 10) -- 10 seconds client timeout
+  s:listen(
+    port,
+    function (connection)
+
+      local function onReceive(connection, request)
+        collectgarbage()
+        local _, method, uri
+        -- FIXME: skip query string
+        _, _, method, uri = request:find("^([A-Z]+) /(.-) HTTP/[1-9]+.[0-9]+\r\n")
+        --r.uri = parseUri(r.request)
+        --print("Method: "..method)
+        --print("Uri: "..uri)
+
+        if method == "PUT" and uri ~= "" then
+          file.close()
+          if not file.open("temp/put", "w") then
+            connection:send("HTTP/1.1 500 Error\r\nConnection: close\r\n\r\nCreate error\n")
+            connection:close()
+          else
+            -- Track Content-Length so we know when we're done
+            local contentLength = tonumber(string.match(request, "\r\nContent%-Length: (%S+)\r\n"))
+
+            local function done(connection)
+              file.close()
+              if contentLength <= 0 then
+                file.remove(uri)
+                file.rename("temp/put", uri)
+              else
+                file.remove("temp/put")
+              end
+            end
+            connection:on("disconnection", done)
+
+            -- From now on, send everything to the file
+            local function writeFile(connection, payload)
+              --print("writeFile")
+              if file.write(payload) then
+                contentLength = contentLength - #payload
+                if contentLength <= 0 then
+                  connection:send("HTTP/1.1 200 OK\r\nConnection: close\r\n\r\nSaved\n")
+                  connection:close()
+                end
+              else
+                connection:send("HTTP/1.1 500 Error\r\nConnection: close\r\n\r\nWrite error\n")
+                connection:close()
+              end
+            end
+            connection:on("receive", writeFile)
+
+            if request:find("\r\nExpect: 100-continue\r\n", 1, true) then
+              -- Send 100 Continue if the client expects it
+              connection:send("HTTP/1.1 100 Continue\r\nConnection: close\r\n\r\n")
+            else
+              -- Find the start of the body (if any)
+              local body_start = request:find("\r\n\r\n", 1, true) + 4
+              writeFile(connection, request:sub(body_start))
+            end
+          end
+        elseif method == "DELETE" then
+          -- Delete the file from flash
+          file.remove(uri)
+          connection:send("HTTP/1.1 200 OK\r\nConnection: close\r\n\r\nDeleted\n")
+          connection:close()
+        elseif method == "POST" then
+          if uri == "" then
+            -- Reboot the device
+            connection:send("HTTP/1.1 200 OK\r\nConnection: close\r\n\r\nRebooting\n")
+            connection:close()
+            node.restart()
+          else
+            -- Poor man's CGI
+            local func = dofile(uri)
+            if func then
+              func(connection, request)
+              connection:on("receive", func)
+            else
+              connection:send("HTTP/1.1 200 OK\r\nConnection: close\r\n\r\n")
+              connection:close()
+            end
+          end
+        elseif method == "GET" then
+          if uri == "" then
+            -- Send the file listing to the client
+            connection:send("HTTP/1.1 200 OK\r\nConnection: close\r\n\r\n")
+            for k,v in pairs(file.list()) do
+              connection:send(k)
+              connection:send("\t")
+              connection:send(v)
+              connection:send("\n")
+            end
+            connection:close()
+          else
+            -- Send the file contents to the client
+            file.close()
+            if not file.open(uri, "r") then
+              connection:send("HTTP/1.1 404 Not Found\r\nConnection: close\r\n\r\nFile not found\n")
+              connection:close()
+            end
+
+            local function onSent(connection)
+              local c = file.read(500)
+              if c then
+                connection:send(c)
+              else
+                file.close()
+                connection:close()
+              end
+            end
+            connection:on("sent", onSent)
+            connection:send("HTTP/1.1 200 OK\r\nConnection: close\r\n\r\n")
+          end
+        else
+          connection:send("HTTP/1.1 501 Not Implemented\r\nConnection: close\r\n\r\n")
+          connection:close()
+        end
+      end
+
+      connection:on("receive", onReceive)
+
+    end
+  )
+
+  -- false and nil evaluate as false
+  local ip = wifi.sta.getip()
+  if not ip then ip = wifi.ap.getip() end
+  print("nodemcu-httpserver running at http://" .. ip .. ":" ..  port)
+  return s
+
+end
---
+++ init.lua
@@ -0,0 +1,101 @@
+-- Begin WiFi configuration
+
+local wifiConfig = {}
+
+-- wifi.STATION         -- station: join a WiFi network
+-- wifi.SOFTAP          -- access point: create a WiFi network
+-- wifi.wifi.STATIONAP  -- both station and access point
+wifiConfig.mode = wifi.STATION  -- both station and access point
+
+wifiConfig.accessPointConfig = {}
+wifiConfig.accessPointConfig.ssid = "ESP-"..node.chipid()   -- Name of the SSID you want to create
+wifiConfig.accessPointConfig.pwd = "ESP-"..node.chipid()    -- WiFi password - at least 8 characters
+
+wifiConfig.accessPointIpConfig = {}
+wifiConfig.accessPointIpConfig.ip = "192.168.111.1"
+wifiConfig.accessPointIpConfig.netmask = "255.255.255.0"
+wifiConfig.accessPointIpConfig.gateway = "192.168.111.1"
+
+wifiConfig.stationPointConfig = {}
+--wifiConfig.stationPointConfig.ssid = "dsl"        -- Name of the WiFi network you want to join
+--wifiConfig.stationPointConfig.pwd =  "0xdeadbeef"                -- Password for the WiFi network
+wifiConfig.stationPointConfig.ssid = "Brinc_Teams"        -- Name of the WiFi network you want to join
+wifiConfig.stationPointConfig.pwd =  "innov8hk"                -- Password for the WiFi network
+
+-- Tell the chip to connect to the access point
+
+wifi.setmode(wifiConfig.mode)
+print('set (mode='..wifi.getmode()..')')
+
+if (wifiConfig.mode == wifi.SOFTAP) or (wifiConfig.mode == wifi.STATIONAP) then
+    print('AP MAC: ',wifi.ap.getmac())
+    wifi.ap.config(wifiConfig.accessPointConfig)
+    wifi.ap.setip(wifiConfig.accessPointIpConfig)
+end
+if (wifiConfig.mode == wifi.STATION) or (wifiConfig.mode == wifi.STATIONAP) then
+    print('Client MAC: ',wifi.sta.getmac())
+    wifi.sta.config(wifiConfig.stationPointConfig.ssid, wifiConfig.stationPointConfig.pwd, 1)
+end
+
+print('chip: ',node.chipid())
+print('heap: ',node.heap())
+
+wifiConfig = nil
+collectgarbage()
+
+-- End WiFi configuration
+
+-- Compile server code and remove original .lua files.
+-- This only happens the first time afer the .lua files are uploaded.
+
+local compileAndRemoveIfNeeded = function(f)
+   if file.open(f) then
+      file.close()
+      print('Compiling:', f)
+      node.compile(f)
+      file.remove(f)
+      collectgarbage()
+   end
+end
+
+local serverFiles = {'httpserver.lua'}
+for i, f in ipairs(serverFiles) do compileAndRemoveIfNeeded(f) end
+
+compileAndRemoveIfNeeded = nil
+serverFiles = nil
+collectgarbage()
+
+-- Connect to the WiFi access point.
+-- Once the device is connected, you may start the HTTP server.
+
+if (wifi.getmode() == wifi.STATION) or (wifi.getmode() == wifi.STATIONAP) then
+    local joinCounter = 0
+    local joinMaxAttempts = 5
+    tmr.alarm(0, 3000, 1, function()
+       local ip = wifi.sta.getip()
+       if ip == nil and joinCounter < joinMaxAttempts then
+          print('Connecting to WiFi Access Point ...')
+          joinCounter = joinCounter +1
+       else
+          if joinCounter == joinMaxAttempts then
+             print('Failed to connect to WiFi Access Point.')
+          else
+            print('IP: ',ip)
+            dofile("httpserver.lc")(80)
+          end
+          tmr.stop(0)
+          joinCounter = nil
+          joinMaxAttempts = nil
+          collectgarbage()
+       end
+    end)
+end
+
+-- Uncomment to automatically start the server in port 80
+if (not not wifi.sta.getip()) or (not not wifi.ap.getip()) then
+    --dofile("httpserver.lc")(80)    
+end
+
+function ls()
+  for k,v in pairs(file.list()) do print(k,v) end
+end
